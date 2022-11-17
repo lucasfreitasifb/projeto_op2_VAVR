408d43795d28c438ccc86f4b01c024ebf4834f70
Lucas Freitas <lucasfreitascj@hotmail.com>
2022-11-17 09:58:44 -0300

    Subindo arquivos

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene("aula");
+    }
+
+    public void GoToTeste(){
+        SceneManager.LoadScene("TesteMenu");
+    }
+
+    public void GoToMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
