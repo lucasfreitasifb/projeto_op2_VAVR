69aad400817060ee9a8e78c1032a33ab82050aca
Lucas Freitas <lucasfreitascj@hotmail.com>
2022-12-01 11:50:51 -0300

    Atualizando arquivos git

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene("Introducao");
+    }
+
+    public void GoToSobre(){
+        SceneManager.LoadScene("Sobre");
+    }
+
+    public void GoToMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
